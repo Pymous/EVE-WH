146b29e231a51575343759b9861f7940d0787102
Pymous <pymous.studio@gmail.com>
2025-02-23 00:33:09 +0100

    Added Tippy.JS for some popover action, and create a MutationObserver to make sure we can use them on the spot

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
+
+// Tippy
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+window.tippy = tippy;
+
+const observer = new MutationObserver(() => {
+    tippy("[data-tippy-content]", {
+        content(reference) {
+            return reference.getAttribute("data-tippy-content");
+        },
+        allowHTML: true,
+    });
+});
+
+observer.observe(document.body, {
+    childList: true,
+    subtree: true,
+});
+
+tippy("[data-tippy-content]", {
+    content(reference) {
+        return reference.getAttribute("data-tippy-content");
+    },
+});
