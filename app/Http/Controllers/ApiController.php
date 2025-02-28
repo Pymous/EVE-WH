4ad3eee7386bcc6c6e4e339f187f5f12f0b69965
Pymous <pymous.studio@gmail.com>
2025-02-28 23:24:23 +0100

    Added $ores[][id] in the solverOres array to be able to filter it by it later

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -55,6 +55,7 @@ class ApiController
                 continue;
             }
             $ores[$item->name] = [
+                'id' => $item->id,
                 'cost' => $item->prices->jita['sell']['percentile'],
                 'm3' => $item->volume,
             ];
@@ -65,5 +66,21 @@ class ApiController
         }
 
         return response()->json($ores);
+    }
+
+    public function getOresList()
+    {
+        $marketGroups = [3638, 3639, 3640, 518, 519, 515, 516, 526, 523, 529, 528, 527, 525, 522, 521, 514, 512, 517, 2538, 2539, 2540, 530, 3487, 3488, 3489, 3490, 1855, 772, 792, 614, 2814, 2396, 2397, 2398, 2400, 2401, 20, 3636, 3637];
+
+        // Get all the items that are in the market groups and the name start with Compressed%
+        $items = Item::whereIn('market_group_id', $marketGroups)->where('name', 'like', 'Compressed%')->with(['prices', 'refinedMaterials'])->get();
+
+        // Make a list of just the names and return that
+        $ores = [];
+        foreach ($items as $item) {
+            $ores[] = $item->name;
+        }
+
+        return $ores;
     }
 }
